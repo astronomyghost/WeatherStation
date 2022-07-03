80f50889069489544d4175b78b1679b194c5d154
lucas <lucasdbarclay@gmail.com>
2022-07-03 23:16:57 +0100

    Prediction update - Hourly temperature basic

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from PIL import Image
 import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
-import math
+import math, requests, json, time
+from csv import writer
+
+#Test site for weather forecasting
 
 class CloudCover:
     def __init__(self, fileName):
@@ -67,8 +70,10 @@ class prediction:
         YY = np.sum(self.y_train**2)
         coefficent = ((self.n*XY)-(np.sum(self.x_train)*np.sum(self.y_train)))/math.sqrt(((self.n*XX)-XX)*((self.n*YY)-YY))
         return coefficent
-    def hourPrediction(self, timeAfterHour):
-
+    def hourPrediction(self, dataset, timeAfterHour):
+        dataset.linearRegression("ID", "Temperature", 60)
+        predictedTemp = self.m * timeAfterHour + self.c
+        print("Predicted temperature after "+str(timeAfterHour)+" minutes is "+str(predictedTemp)+" degrees celsius")
 
 # Load image and begin simple analysis
 skyShot = CloudCover("Clouds1.jpg")
@@ -78,8 +83,31 @@ condition = skyShot.determineCondition()
 print(condition, skyShot.timestamp)
 
 # OpenCV stuff
-dataset = prediction("TestHourlyData.csv")
-dataset.linearRegression("ID", "Cloud cover",30)
+while True:
+    dataset = prediction("TestHourlyData.csv")
+    BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+    CITY = "London"
+    API_KEY = "10bc2d9b9c3edc8216ea0c7e49460849"
+
+    URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
+    response = requests.get(URL)
+    dataset.hourPrediction(dataset, 60)
+    if response.status_code == 200:
+        data = response.json()
+        main = data['main']
+        temperature = main['temp']-273
+        weatherData = pd.read_csv("TestHourlyData.csv")
+        n = len(weatherData.loc[: "ID"])
+        with open("TestHourlyData.csv", 'a', newline='') as file:
+            csvWriter =  writer(file)
+            csvWriter.writerow([n+1, temperature, 0, 0])
+        file.close()
+    time.sleep(60)
+
+data = response.json()
+main = data['main']
+temperature = main['temp']
+print(temperature-273)
 
 #Calculating correlation coefficient
 r = dataset.correlationCoefficient()
