1591c1854446c5e6f8171d2c923fdc1cef2f958f
lucas <lucasdbarclay@gmail.com>
2022-07-24 21:49:51 +0100

    User database

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from Prediction import *
 from flask import *
+import sqlite3 as sql
 
+conn = sql.connect('Users.db', check_same_thread=False)
+cur = conn.cursor()
 app = Flask(__name__)
 
 # Load image and begin simple analysis
@@ -26,16 +29,36 @@ def loginPage():
 @app.route('/LoginReceiver', methods=['POST', 'GET'])
 def loginRequest():
     if request.method == 'POST':
-        userName = request.form['userName']
-        return redirect(url_for('userPage', user=userName))
+        username = request.form['username']
+        return redirect(url_for('userPage', user=username))
     else:
-        userName = request.args.get('userName')
-        return redirect(url_for('userPage', user=userName))
+        username = request.args.get('username')
+        return redirect(url_for('userPage', user=username))
 
 @app.route('/RegisterReceiver', methods=['POST', 'GET'])
 def registerRequest():
     if request.method == 'POST':
+        username = request.form['username']
         password = request.form['password']
+        checkPassword = request.form['checkPassword']
+        cur.execute("SELECT UserID FROM RegisteredUsers WHERE Username=?", (username,))
+        if password == checkPassword and len(cur.fetchall()) < 1:
+            cur.execute("INSERT INTO RegisteredUsers (Username, Password, ImageCount) VALUES (?, ?, 0)", (username, password))
+            conn.commit()
+            return redirect(url_for('userPage', user=username))
+        else:
+            return redirect(url_for('loginPage'))
+    else:
+        username = request.args.get('username')
+        password = request.args.get('password')
+        checkPassword = request.args.get('checkPassword')
+        cur.execute("SELECT UserID FROM RegisteredUsers WHERE Username=?", (username,))
+        if password == checkPassword and len(cur.fetchall()) < 1:
+            cur.execute("INSERT INTO RegisteredUsers (Username, Password, ImageCount) VALUES (?, ?, 0)", (username, password))
+            conn.commit()
+            return redirect(url_for('userPage', user=username))
+        else:
+            return redirect(url_for('loginPage'))
 
 if __name__ == "__main__":
     app.run(debug=False)
